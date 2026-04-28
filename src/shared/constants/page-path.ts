87ab69def66413a6b8f1ec97e4144747f0b2a4b9
CandraJengger <cakjengger@gmail.com>
2026-04-28 16:55:18 +0700

    feat: implement shared project utilities including custom hooks, constants, and API configuration

---
+++ src/shared/constants/page-path.ts
@@ -0,0 +1,4 @@
+export const PAGE_PATH = Object.freeze({
+  LOGIN: '/login',
+  DASHBOARD: '/',
+});
---
+++ src/shared/hooks/use-fetcher.ts
@@ -0,0 +1,42 @@
+import { useAxios } from './use-axios';
+import { COOKIES_KEY } from '../constants';
+import { useCookies } from './use-cookies';
+import { ENV } from '../configs';
+
+export function useFetcher() {
+  const cookies = useCookies();
+  const axiosInstance = useAxios({
+    headers: {
+      Authorization: `Bearer ${cookies.get(COOKIES_KEY.TOKEN)}`,
+    },
+  });
+
+  axiosInstance.interceptors.request.use(
+    async (config) => {
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    },
+  );
+
+  axiosInstance.interceptors.response.use(
+    (response) => {
+      // if (ENV.DEBUG_MODE) {
+      //   console.log(
+      //     `${response.config.method?.toUpperCase()} ${response.config.url} - ${
+      //       response.status
+      //     } - ${JSON.stringify(response.data, null, 4).substring(0, 1000)}`,
+      //   );
+      // }
+      return response;
+    },
+    (error) => {
+      return new Promise((resolve, reject) => {
+        reject(error);
+      });
+    },
+  );
+
+  return axiosInstance;
+}
